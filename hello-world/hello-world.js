461bb5da8dde0fa0a9e229a25b50f75d342dc9c7
Conni Simmons <connisimmons@gmail.com>
2020-08-16 19:52:10 -0600

    removing robot

--- hello-world/hello-world.js
+++ hello-world/hello-world.js
@@ -1,8 +1,17 @@
-//
-// This is only a SKELETON file for the 'Hello World' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-export const hello = () => {
-  return ('Hello, World!')
-};
+export function hello(array) {
+  let n = array.length;
+  while (n > 0) {
+    let lastModifiedIndex = 0;
+    for (let currentIndex = 1; currentIndex < n; currentIndex++) {
+      // if the item at the previous index is greater than the item at the `currentIndex`, swap them
+      if (array[currentIndex - 1] > array[currentIndex]) {
+        // swap
+        let temp = array[currentIndex - 1];
+        array[currentIndex - 1] = array[currentIndex];
+        array[currentIndex] = temp; // save the index that was modified
+        lastModifiedIndex = currentIndex;
+      }
+    } // save the last modified index so we know not to iterate past it since all proceeding values are sorted
+    n = lastModifiedIndex;
+  }
+}
--- hello-world/hello-world.spec.js
+++ hello-world/hello-world.spec.js
@@ -1,7 +1,16 @@
-import { hello } from './hello-world';
+import { hello } from './hello-world'
 
-describe('Hello World', () => {
-  test('Say Hi!', () => {
-    expect(hello()).toEqual('Hello, World!');
-  });
-});
+describe('hello()', () => {
+    test('works with empty array', () => {
+        expect(hello()).toEqual([])
+    })
+    xtest('works with one number', () => {
+        expect(hello([1])).toEqual([1])
+    })
+    xtest('sorts numbers', () => {
+        expect(hello([ 5, 4, 3, 2, 1])).toEqual([1, 2, 3, 4, 5])
+    })
+})
+    
+
+    
